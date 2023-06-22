23f585429d58e463265c8677cfd949f96c6513cd
GSSachete <gabrielasachete@outlook.com>
2023-06-21 23:44:06 -0300

    SacheteJAo Bootstrapproduto

--- view/produto/produto_inserir.php
+++ view/produto/produto_inserir.php
@@ -12,7 +12,7 @@
             margin-top: 5%;
         }
     </style>
-    <title>Cadastrar Cliente</title>
+    <title>Cadastrar produto</title>
 </head>
 <body>
     <div class="dsp-flex justify-content-center">
@@ -20,70 +20,43 @@
             <div class="container shadow pb-2">
             <div class="col-md-12 container dsp-flex justify-content-between position-relative progress-box bg-dark rounded mt-2">
                 <div class="col-md-12 title-person-2" align="center">
-                    CADASTRAR CLIENTE
+                    CADASTRAR PRODUTO
                 </div>
             </div>
-            <!-- RG-->
+            <!-- ID-->
             <div class="col-md-8 container dsp-flex flex-column justify-content-center">
-                <label for="User" class="form-label">RG/IE</label>
+                <label for="User" class="form-label">ID</label>
                 <input type="text" class="form-control" name="User" id="User">
             </div>
-            <!-- CNPJ -->
+            <!-- NOME -->
             <div class="col-md-8 container dsp-flex flex-column justify-content-center">
-                <label for="Pass" class="form-label">CNPJ/CPF</label>
+                <label for="Pass" class="form-label">NOME</label>
                 <input type="text" class="form-control" name="Pass" id="Pass">
             </div>
-            <!-- TIPO PESSOA -->
+            <!-- VALOR VENDA -->
             <div class="col-md-8 container dsp-flex flex-column justify-content-center">
-            <label for="Pass" class="form-label">Selecione seu tipo pessoa</label>
-            <div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-  <label class="form-check-label" for="flexRadioDefault1">
-    Juridica 
-  </label>
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" >
-  <label class="form-check-label" for="flexRadioDefault2">
-   Fisica
-  </label>
-  </div>
-</div>
-<!-- SEXOOOOOOOOOOOOOOOOOOOO -->
-<div class="col-md-8 container dsp-flex flex-column justify-content-center">
-            <label for="Pass" class="form-label">Sexo </label>
-            <div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-  <label class="form-check-label" for="flexRadioDefault1">
-    Masculino
-  </label>
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" >
-  <label class="form-check-label" for="flexRadioDefault2">
-   Feminino
-  </label>
-  </div>
-  <div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-  <label class="form-check-label" for="flexRadioDefault1">
-    MNETC
-  </label>
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-  <label class="form-check-label" for="flexRadioDefault1">
-    Indefinido
-  </label>
-</div>
-</div>
-<!-- DATA -->
-<div class="col-md-8 container dsp-flex flex-column justify-content-center">
-                <label for="Pass" class="form-label">DATA</label>
-                <input type="text" class="form-control" name="Pass" id="Pass" placeholder="DD/MM/AAAA">
+                <label for="Pass" class="form-label">VALOR VENDA</label>
+                <input type="text" class="form-control" name="Pass" id="Pass" placeholder="R$">
             </div>
+               <!-- VALOR COMPRA -->
+               <div class="col-md-8 container dsp-flex flex-column justify-content-center">
+                <label for="Pass" class="form-label">VALOR COMPRA</label>
+                <input type="text" class="form-control" name="Pass" id="Pass" placeholder="R$">
+            </div>
+              <!-- CODIGO DE BARRA -->
+              <div class="col-md-8 container dsp-flex flex-column justify-content-center">
+                <label for="Pass" class="form-label">Código de Barras/label>
+                <input type="text" class="form-control" name="Pass" id="Pass" placeholder="00000000000">
+            </div>
+              <!-- ESTOQUE -->
+              <div class="col-md-8 container dsp-flex flex-column justify-content-center">
+                <label for="Pass" class="form-label">QUANTAS UNIDADES EM ESTOQUE</label>
+                <input type="text" class="form-control" name="Pass" id="Pass" >
+            </div>
+
+
             <div class="botoes dsp-flex justify-content-end col-md-11">
-                <a id="cadastrar" class="btn btn-large btn-dark" align="right"><i class="fa-sharp fa-solid fa-plus fa-fade"></i> Cadastrar</a>
+                <a id="cadastrar" class="btn btn-large btn-dark" align="right"><i class="fa-sharp fa-solid fa-plus fa-fade"></i> Cadastrar produto</a>
             </div>
             
             </div>
