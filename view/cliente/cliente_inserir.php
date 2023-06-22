2d9e67cd229425ffc62ff7bebcebbe1fd9af7154
MatheusOscary <thelastsurprise2021@outlook.com>
2023-06-22 19:50:03 -0300

    Adicionando value no input

--- view/cliente/cliente_inserir.php
+++ view/cliente/cliente_inserir.php
@@ -25,69 +25,73 @@
             </div>
             <!-- RG-->
             <div class="col-md-8 container dsp-flex flex-column justify-content-center">
-                <label for="User" class="form-label">RG/IE</label>
-                <input type="text" class="form-control" name="User" id="User">
+                <label for="RG_IE" class="form-label">RG/IE</label>
+                <input type="text" class="form-control" name="RG_IE" id="RG_IE">
             </div>
+			<br>
             <!-- CNPJ -->
             <div class="col-md-8 container dsp-flex flex-column justify-content-center">
-                <label for="Pass" class="form-label">CNPJ/CPF</label>
-                <input type="text" class="form-control" name="Pass" id="Pass">
+                <label for="CNPJ_CPF" class="form-label">CNPJ/CPF</label>
+                <input type="text" class="form-control" name="CNPJ_CPF" id="CNPJ_CPF">
             </div>
+			<br>
             <!-- TIPO PESSOA -->
             <div class="col-md-8 container dsp-flex flex-column justify-content-center">
-            <label for="Pass" class="form-label">Selecione seu tipo pessoa</label>
-            <div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-  <label class="form-check-label" for="flexRadioDefault1">
-    Juridica 
-  </label>
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" >
-  <label class="form-check-label" for="flexRadioDefault2">
-   Fisica
-  </label>
-  </div>
-</div>
+            	<label class="form-label">Selecione seu tipo pessoa</label>
+            		<div class="form-check">
+						<label class="form-check-label" for="Tipo_pessoa1">
+							Juridica 
+						</label>
+  						<input class="form-check-input" type="radio" value="J" name="Tipo_pessoa" id="Tipo_pessoa1">
+					</div>
+					<div class="form-check">
+						<input class="form-check-input" type="radio" checked="checked" value="F" name="Tipo_pessoa" id="Tipo_pessoa2" >
+						<label class="form-check-label" for="Tipo_pessoa2">
+							Fisica
+						</label>
+					</div>
+				</label>
+			</div>
+			<br>
 <!-- SEXOOOOOOOOOOOOOOOOOOOO -->
-<div class="col-md-8 container dsp-flex flex-column justify-content-center">
-            <label for="Pass" class="form-label">Sexo </label>
-            <div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-  <label class="form-check-label" for="flexRadioDefault1">
-    Masculino
-  </label>
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" >
-  <label class="form-check-label" for="flexRadioDefault2">
-   Feminino
-  </label>
-  </div>
-  <div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-  <label class="form-check-label" for="flexRadioDefault1">
-    MNETC
-  </label>
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-  <label class="form-check-label" for="flexRadioDefault1">
-    Indefinido
-  </label>
-</div>
-</div>
-<!-- DATA -->
-<div class="col-md-8 container dsp-flex flex-column justify-content-center">
-                <label for="Pass" class="form-label">DATA</label>
-                <input type="text" class="form-control" name="Pass" id="Pass" placeholder="DD/MM/AAAA">
-            </div>
-            <div class="botoes dsp-flex justify-content-end col-md-11">
-                <a id="cadastrar" class="btn btn-large btn-dark" align="right"><i class="fa-sharp fa-solid fa-plus fa-fade"></i> Cadastrar</a>
-            </div>
-            
-            </div>
-            
+			<div class="col-md-8 container dsp-flex flex-column justify-content-center">
+				<label class="form-label">Sexo</label>
+					<div class="form-check">
+						<input class="form-check-input" checked="checked" value="M" type="radio" name="Sexo" id="Sexo1">
+						<label class="form-check-label" for="Sexo1">
+							Masculino
+						</label>
+					</div>
+					<div class="form-check">
+						<input class="form-check-input" type="radio" value="F" name="Sexo" id="Sexo2" >
+						<label class="form-check-label" for="Sexo2">
+							Feminino
+						</label>
+					</div>
+					<div class="form-check">
+						<input class="form-check-input" type="radio" value="MNTC" name="Sexo" id="Sexo3">
+						<label class="form-check-label" for="Sexo3">
+							MNETC
+						</label>
+					</div>
+					<div class="form-check">
+						<input class="form-check-input" type="radio" value="I" name="Sexo" id="Sexo3">
+						<label class="form-check-label" for="Sexo3">
+							Indefinido
+						</label>
+					</div>
+				</div>
+				<br>
+				<!-- DATA -->
+				<div class="col-md-8 container dsp-flex flex-column justify-content-center">
+					<label for="Data_nascimento" class="form-label">Data</label>
+					<input type="date" class="form-control" name="Data_nascimento" id="Data_nascimento" placeholder="DD/MM/AAAA">
+				</div>
+				<br>
+				<div class="botoes dsp-flex justify-content-end col-md-11">
+					<a id="cadastrar" class="btn btn-large btn-dark" align="right"><i class="fa-sharp fa-solid fa-plus fa-fade"></i> Cadastrar</a>
+				</div>
+            </div> 
         </form>
     </div>
 </body>
