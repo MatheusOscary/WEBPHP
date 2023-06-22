71dfd9e7a8c7caf883770c39b7844bd2c2b50306
GSSachete <gabrielasachete@outlook.com>
2023-06-21 23:26:34 -0300

    SacheteJao-CadastroClienteInserir

--- view/cliente/cliente_inserir.php
+++ view/cliente/cliente_inserir.php
@@ -23,18 +23,71 @@
                     CADASTRAR CLIENTE
                 </div>
             </div>
+            <!-- RG-->
             <div class="col-md-8 container dsp-flex flex-column justify-content-center">
-                <label for="User" class="form-label">Nome de usuário</label>
+                <label for="User" class="form-label">RG/IE</label>
                 <input type="text" class="form-control" name="User" id="User">
             </div>
+            <!-- CNPJ -->
             <div class="col-md-8 container dsp-flex flex-column justify-content-center">
-                <label for="Pass" class="form-label">Senha</label>
-                <input type="password" class="form-control" name="Pass" id="Pass">
+                <label for="Pass" class="form-label">CNPJ/CPF</label>
+                <input type="text" class="form-control" name="Pass" id="Pass">
+            </div>
+            <!-- TIPO PESSOA -->
+            <div class="col-md-8 container dsp-flex flex-column justify-content-center">
+            <label for="Pass" class="form-label">Selecione seu tipo pessoa</label>
+            <div class="form-check">
+  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+  <label class="form-check-label" for="flexRadioDefault1">
+    Juridica 
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" >
+  <label class="form-check-label" for="flexRadioDefault2">
+   Fisica
+  </label>
+  </div>
+</div>
+<!-- SEXOOOOOOOOOOOOOOOOOOOO -->
+<div class="col-md-8 container dsp-flex flex-column justify-content-center">
+            <label for="Pass" class="form-label">Sexo </label>
+            <div class="form-check">
+  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+  <label class="form-check-label" for="flexRadioDefault1">
+    Masculino
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" >
+  <label class="form-check-label" for="flexRadioDefault2">
+   Feminino
+  </label>
+  </div>
+  <div class="form-check">
+  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+  <label class="form-check-label" for="flexRadioDefault1">
+    MNETC
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+  <label class="form-check-label" for="flexRadioDefault1">
+    Indefinido
+  </label>
+</div>
+</div>
+<!-- DATA -->
+<div class="col-md-8 container dsp-flex flex-column justify-content-center">
+                <label for="Pass" class="form-label">DATA</label>
+                <input type="text" class="form-control" name="Pass" id="Pass" placeholder="DD/MM/AAAA">
             </div>
             <div class="botoes dsp-flex justify-content-end col-md-11">
                 <a id="cadastrar" class="btn btn-large btn-dark" align="right"><i class="fa-sharp fa-solid fa-plus fa-fade"></i> Cadastrar</a>
             </div>
+            
             </div>
+            
         </form>
     </div>
 </body>
